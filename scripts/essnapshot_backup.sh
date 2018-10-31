964348a8bb971ee0002f87c22caccf3169d2cb8f
garyluu <garylau.work@gmail.com>
2018-10-31 17:27:32 -0400

    Cleanup

--- scripts/essnapshot_backup.sh
+++ scripts/essnapshot_backup.sh
@@ -2,8 +2,9 @@
 set -x
 set -e
 
-cd compose_setup
+# Corresponding cronjob is "0 0 * * * /bin/bash /home/ubuntu/compose_setup/scripts/essnapshot_backup.sh"
 
-aws s3 --endpoint-url https://object.cancercollaboratory.org:9080 sync essnapshot s3://logstash-elasticdata/essnapshot
+curl -X PUT "localhost:9200/_snapshot/my_backup/%3Csnapshot-%7Bnow%2Fd%7D%3E"
+aws s3 --endpoint-url https://object.cancercollaboratory.org:9080 sync /home/ubuntu/compose_setup/essnapshot s3://logstash-elasticdata/essnapshot
 
-echo "Uploaded essnapshot to s3://oicr.backups.dockstore/staging.dockstore.org/essnapshot"
+echo "Uploaded essnapshot to https://object.cancercollaboratory.org:9080 s3://logstash-elasticdata/essnapshot"
