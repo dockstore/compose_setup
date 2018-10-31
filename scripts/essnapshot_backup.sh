ba3fbdec37a2f2092d6d355e9772886c20b0229b
garyluu <garylau.work@gmail.com>
2018-10-31 17:13:50 -0400

    Change backup to use cancercollab

--- scripts/essnapshot_backup.sh
+++ scripts/essnapshot_backup.sh
@@ -3,8 +3,7 @@ set -x
 set -e
 
 cd compose_setup
-s3_url_essnapshot_backup='s3://oicr.backups.dockstore/staging.dockstore.org/essnapshot'
 
-aws s3 --region us-east-1 cp essnapshot $s3_url_essnapshot_backup/ --recursive
+aws s3 --endpoint-url https://object.cancercollaboratory.org:9080 sync essnapshot s3://logstash-elasticdata/essnapshot
 
 echo "Uploaded essnapshot to s3://oicr.backups.dockstore/staging.dockstore.org/essnapshot"
