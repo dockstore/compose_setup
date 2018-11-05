34dc9245c0db417238d98db132f69197a1d04c8d
garyluu <garylau.work@gmail.com>
2018-11-05 11:39:25 -0500

    Add monthly backup

--- templates/essnapshot_backup.sh
+++ templates/essnapshot_backup.sh
@@ -6,7 +6,9 @@ WEBHOOK_URL='{{SLACK_URL}}'
 today=`date +%Y-%m-%d.%H:%M:%S`
 mkdir -p /home/ubuntu/compose_setup/logs
 filename="/home/ubuntu/compose_setup/logs/$today"
-# Corresponding cronjob is "0 0 * * * /bin/bash /home/ubuntu/compose_setup/scripts/essnapshot_backup.sh"
+# Corresponding cronjobs are:
+# "0 0 * * * /bin/bash /home/ubuntu/compose_setup/scripts/essnapshot_backup.sh essnapshot"
+# "0 0 1 * * /bin/bash /home/ubuntu/compose_setup/scripts/essnapshot_backup.sh essnapshot_monthly"
 
 curator --config /home/ubuntu/compose_setup/curator/curator.yml /home/ubuntu/compose_setup/curator/delete_old_snapshots.yml
 curl -X PUT "localhost:9200/_snapshot/my_backup/%3Csnapshot-%7Bnow%2Fd%7D%3E" | grep accepted\":true
@@ -14,7 +16,7 @@ if [ $? -ne 0 ]
 then
 	curl -X POST -H 'Content-type: application/json' --data '{"text":"Taking snapshot failed."}' $WEBHOOK_URL
 else
-	aws s3 --endpoint-url https://object.cancercollaboratory.org:9080 cp --recursive /home/ubuntu/compose_setup/essnapshot s3://logstash-elasticdata/essnapshot > $filename
+	aws s3 --endpoint-url https://object.cancercollaboratory.org:9080 cp --recursive /home/ubuntu/compose_setup/essnapshot s3://logstash-elasticdata/$1 > $filename
 	if [ $? -ne 0 ]
 	then
 		curl -X POST -H 'Content-type: application/json' --data '{"text":"Sending snapshot to s3 failed."}' $WEBHOOK_URL
