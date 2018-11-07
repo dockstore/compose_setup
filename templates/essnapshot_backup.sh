29f9751964d1216210c584654435330997d624c3
garyluu <garylau.work@gmail.com>
2018-11-07 12:59:44 -0500

    Fix

--- templates/essnapshot_backup.sh
+++ templates/essnapshot_backup.sh
@@ -8,7 +8,7 @@ dayofmonth=`date +%d`
 mkdir -p /home/ubuntu/compose_setup/logs
 filename="/home/ubuntu/compose_setup/logs/$today"
 # Corresponding cronjobs are:
-# "0 0 * * * /bin/bash /home/ubuntu/compose_setup/scripts/essnapshot_backup.sh"
+# "0 0 * * * /bin/bash /home/ubuntu/compose_setup/scripts/essnapshot_backup.sh &> /home/ubuntu/compose_setup/logs/`/bin/date +\%Y-\%m-\%d.\%H:\%M:\%S`-cron.log"
 
 curator --config /home/ubuntu/compose_setup/curator/curator.yml /home/ubuntu/compose_setup/curator/delete_old_snapshots.yml
 curl -X PUT "localhost:9200/_snapshot/my_backup/%3Csnapshot-%7Bnow%2Fd%7D%3E" | grep accepted\":true
@@ -18,7 +18,7 @@ then
 else
 	cd /home/ubuntu/compose_setup
 	zip -r essnapshot-$dayofmonth.zip essnapshot
-	aws s3 --endpoint-url https://object.cancercollaboratory.org:9080 cp /home/ubuntu/compose_setup/essnapshot-$dayofmonth.zip s3://logstash-elasticdata/essnapshot-$dayofmonth.zip > $filename
+	/home/ubuntu/.local/bin/aws s3 --endpoint-url https://object.cancercollaboratory.org:9080 cp /home/ubuntu/compose_setup/essnapshot-$dayofmonth.zip s3://logstash-elasticdata/essnapshot-$dayofmonth.zip
 	if [ $? -ne 0 ]
 	then
 		curl -X POST -H 'Content-type: application/json' --data '{"text":"Sending snapshot to s3 failed."}' $WEBHOOK_URL
