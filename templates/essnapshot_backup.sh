1f925d864ec4637699a572b5c4b256b3fc4deb39
garyluu <garylau.work@gmail.com>
2019-01-21 10:07:33 -0500

    Use absolute paths

--- templates/essnapshot_backup.sh
+++ templates/essnapshot_backup.sh
@@ -8,7 +8,7 @@ mkdir -p /home/ubuntu/compose_setup/logs
 # "0 0 * * * /bin/bash /home/ubuntu/compose_setup/scripts/essnapshot_backup.sh daily &> /home/ubuntu/compose_setup/logs/`/bin/date +\%Y-\%m-\%d.\%H:\%M:\%S`-cron.log"
 # "15 0 * * 0 /bin/bash /home/ubuntu/compose_setup/scripts/essnapshot_backup.sh weekly &> /home/ubuntu/compose_setup/logs/`/bin/date +\%Y-\%m-\%d.\%H:\%M:\%S`-cron.log"
 # "30 0 1 * * /bin/bash /home/ubuntu/compose_setup/scripts/essnapshot_backup.sh monthly &> /home/ubuntu/compose_setup/logs/`/bin/date +\%Y-\%m-\%d.\%H:\%M:\%S`-cron.log"
-curator --config /home/ubuntu/compose_setup/curator/curator.yml /home/ubuntu/compose_setup/curator/delete_old_snapshots.yml
+/home/ubuntu/.local/bin/curator --config /home/ubuntu/compose_setup/curator/curator.yml /home/ubuntu/compose_setup/curator/delete_old_snapshots.yml
 curl -X PUT "localhost:9200/_snapshot/my_backup/%3Csnapshot-%7Bnow%2Fd%7D%3E?wait_for_completion=true" | grep accepted\":true
 if [ $? -ne 0 ]
 then
@@ -18,7 +18,7 @@ fi
 
 cd /home/ubuntu/compose_setup
 zip -r essnapshot-$1.zip essnapshot
-previoussize=`aws s3 --endpoint-url https://object.cancercollaboratory.org:9080 ls s3://logstash-elasticdata/essnapshot-$1.zip --summarize | grep 'Total Size:' | awk '{print $3}'`
+previoussize=`/home/ubuntu/.local/bin/aws s3 --endpoint-url https://object.cancercollaboratory.org:9080 ls s3://logstash-elasticdata/essnapshot-$1.zip --summarize | grep 'Total Size:' | awk '{print $3}'`
 currentsize=`stat --printf="%s" essnapshot-$1.zip`
 
 if [ $previoussize -gt $currentsize ]
