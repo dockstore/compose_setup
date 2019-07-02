ecc034f8f54b945d78026de8e95b15baa9a78d8a
Denis Yuen <denis.yuen@oicr.on.ca>
2019-07-02 15:53:03 -0400

    Update postgres_backup.sh

--- scripts/postgres_backup.sh
+++ scripts/postgres_backup.sh
@@ -8,7 +8,7 @@ s3_url_config_backup='s3://oicr.backups.dockstore/staging.dockstore.org/config/'
 temp_dir=`mktemp -d`
 output_file=ds-webservice_${2:-prod}_`date +%Y-%m-%dT%H-%M-%S%z`.sql
 
-/usr/local/bin/docker-compose exec -T --user postgres postgres pg_dump > $temp_dir/$output_file
+/usr/local/bin/docker-compose exec -T --user postgres postgres pg_dump --no-owner > $temp_dir/$output_file
 
 aws s3 --region us-east-1 cp $temp_dir/$output_file $s3_url_db_backup/
 aws s3 --region us-east-1 cp /home/ubuntu/compose_setup/dockstore_launcher_config/compose.config $s3_url_config_backup/
