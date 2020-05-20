eb029bef188f1222c8cb89132349d437964b5246
NatalieEO <nperez9@ucsc.edu>
2020-05-20 15:05:07 -0700

    pr feedback

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,2 +1,11 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# Restarts stack by running commands from /home/ubuntu/compose_setup
+
+cd /home/ubuntu/compose_setup
 docker-compose down
 nohup docker-compose up --force-recreate --remove-orphans >/dev/null 2>&1 &
