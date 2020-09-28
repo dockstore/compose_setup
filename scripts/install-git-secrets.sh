7620a1fb776a11924b9840d7e474b7f8578c77eb
Louise Cabansay <ldcabansay@gmail.com>
2020-09-28 10:19:59 -0700

    get install script from UI, for CI use same version of node as UI (10.13)

--- scripts/install-git-secrets.sh
+++ scripts/install-git-secrets.sh
@@ -11,70 +11,8 @@ set -u
 set -o xtrace
 #to turn off echo do 'set +o xtrace'
 
-get_OS_type()
-{
-  unameOut="$(uname -s)"
-  case "${unameOut}" in
-      Linux*)     machine=Linux;;
-      Darwin*)    machine=Mac;;
-      CYGWIN*)    machine=Cygwin;;
-      MINGW*)     machine=MinGw;;
-      *)          machine="UNKNOWN:${unameOut}"
-  esac
-  echo "Identified OS as ${machine}"
-}
-
-install_git_secrets_from_source()
-{
-  echo "Installing git secrets from source"
-  wget --no-verbose -O git-secrets-1.3.0.tar.gz https://github.com/awslabs/git-secrets/archive/1.3.0.tar.gz
-  tar -zxf git-secrets-1.3.0.tar.gz
-  cd git-secrets-1.3.0
-  sudo make install
-}
-
-install_git_secrets_with_Brew()
-{
-  echo "Installing git secrets with Brew"
-  # install git secrets using brew
-  brew install git-secrets
-}
-
-# if git secrets is not installed
-if ! which git-secrets &>/dev/null
-then
-  echo "git secrets is not installed"
-  get_OS_type
-  # if the OS is Mac
-  if [[ ${machine} == "Mac" ]]
-  then
-    # if brew is installed
-    if which brew &>/dev/null
-    then
-      install_git_secrets_with_Brew
-    else
-      echo "Brew not present so cannot install git secrets"
-    fi
-  elif [[ ${machine} == "Linux" ]]
-  then
-    install_git_secrets_from_source
-  else
-    echo "Unsupported OS for git secrets install"
-  fi
-else
-  echo "git secrets is already installed"
-fi
-
-# if git secrets is now installed
-if which git-secrets &>/dev/null
-then
-  # setup the AWS specific configuration variables
-  # and custom patterns
-  git secrets --register-aws
-  # return success code (true) since git secrets --add
-  # will return a 1 if the pattern is already set
-  # and that will fail when running
-  # npm run install-git-secrets
-  git secrets --add '[a-fA-F0-9]{30,}' || true
-  git secrets --scan
-fi
+#get install git-secrets script from UI
+GIT_SECRETS_SOURCE="https://raw.githubusercontent.com/dockstore/dockstore-ui2/develop/scripts/install-git-secrets.sh"
+wget -O git-secrets.sh --no-verbose --tries=10 ${GIT_SECRETS_SOURCE}
+chmod u+x git-secrets.sh
+bash git-secrets.sh
